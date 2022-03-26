77565c6172bbf0a52d4f9192bee204915ec889c8
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 19:28:31 +0430

    Sample routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Blogs from "./pages/blogs";
+import BlogPage from "./components/blogpage";
+import New from "./pages/new";
 
 function App() {
     return (
-        <React.Fragment>
-            <p>Hi</p>
-        </React.Fragment>
+        <Router>
+            <Switch>
+                <Route exact path='/'>Index</Route>
+                <Route exact path='/blogs'><Blogs /></Route>
+                <Route path='/blog/:id'><BlogPage /></Route>
+                <Route exact path='/new'><New /></Route>
+            </Switch>
+        </Router>
     );
 }
 
