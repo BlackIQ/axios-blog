070c341cfebc408309cefbb15d86a0ab84af9134
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 21:25:45 +0430

    Added container and full layout

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Blogs from "./pages/blogs";
 import BlogPage from "./components/blogpage";
 import New from "./pages/new";
+import Navbar from "./components/navbar";
 
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route exact path='/'>Index</Route>
-                <Route exact path='/blogs'><Blogs /></Route>
-                <Route path='/blog/:id'><BlogPage /></Route>
-                <Route exact path='/new'><New /></Route>
-            </Switch>
+            <Navbar />
+            <div className='container py-4'>
+                <Switch>
+                    <Route exact path='/'>Index</Route>
+                    <Route exact path='/blogs'><Blogs /></Route>
+                    <Route path='/blog/:id'><BlogPage /></Route>
+                    <Route exact path='/new'><New /></Route>
+                </Switch>
+            </div>
         </Router>
     );
 }
