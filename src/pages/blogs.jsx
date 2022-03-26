5b5206ee54a0ace04a527914675d11cdcc1769d0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 22:34:12 +0430

    ADded loading

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -6,19 +6,21 @@ const Blogs = () => {
     const [data, setData] = useState('');
 
     useEffect(() => {
-        axios.get('http://localhost:8000/blogs').then((response) => {setData(response.data)});
-    }, []);
-
-    if (!data) return null;
+        axios.get('http://localhost:8000/blogs').then((response) => {
+            setData(response.data)
+        });
+    });
 
     return (
         <div>
             {
-                data.map((d) => {
+                data ? data.sort((a, b) => {
+                    return a.first > b.first ? 1 : -1
+                }).map((d) => {
                     return (
-                        <BlogItem key={d.id} blog={d} />
+                        <BlogItem key={d.id} blog={d}/>
                     );
-                })
+                }) : <p>Loading . . .</p>
             }
         </div>
     );
