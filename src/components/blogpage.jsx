cc1ce84a72ffab6db2ec5af4dda9766629c8e39a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 22:34:30 +0430

    Added loading

--- src/components/blogpage.jsx
+++ src/components/blogpage.jsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 
 const BlogPage = () => {
-    const [data, setData] = useState('');
-
     const {id} = useParams();
+
+    const [data, setData] = useState('');
 
     const deleteBlog = id => {
         axios.delete('http://localhost:8000/blogs/' + id).then(response => {
@@ -17,20 +17,22 @@ const BlogPage = () => {
         axios.get('http://localhost:8000/blogs/' + id).then((response) => {
             setData(response.data)
         });
-    }, []);
-
-    if (!data) return null;
+    });
 
     return (
         <div>
-            <h1>{data.title}</h1>
-            <br/>
-            <p>{data.text}</p>
-            <hr />
-            <p>
-                <span onClick={(id) => {deleteBlog(data.id)}} className={'text-danger pointer'}>Delete</span>
-                <span onClick={(id) => {deleteBlog(data.id)}} className={'text-primary pointer float-end'}>Edit</span>
-            </p>
+            {
+                data ? <div>
+                    <h1>{data.title}</h1>
+                    <br/>
+                    <p>{data.text}</p>
+                    <hr />
+                    <p>
+                        <span onClick={(id) => {deleteBlog(data.id)}} className={'text-danger pointer'}>Delete</span>
+                        <Link to={'/edit/' + data.id} className={'text-primary pointer float-end'}>Edit</Link>
+                    </p>
+                </div> : <p>Loading . . .</p>
+            }
         </div>
     );
 }
