7edebdd64f2a1c89ca4fb8e50c0a6c0b4f81987a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 21:25:22 +0430

    Fix title and text

--- src/components/blogitem.jsx
+++ src/components/blogitem.jsx
@@ -3,7 +3,13 @@ import {Link} from "react-router-dom";
 const BlogItem = (props) => {
     const blog = props.blog;
     return (
-        <p><Link to={'/blog/' + blog.id}>{blog.name}</Link></p>
+        <div>
+            <div className='border p-3 rounded-3'>
+                <h2><Link to={'/blog/' + blog.id}>{blog.title}</Link></h2>
+                <p>{blog.text}</p>
+            </div>
+            <br />
+        </div>
     );
 }
 
