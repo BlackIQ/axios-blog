21a007c4be3b40c4adca4a577c2c54f87b56d6ec
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 21:26:48 +0430

    Added new post

--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -1,6 +1,43 @@
+import {useState} from "react";
+import axios from "axios";
+
 const New = () => {
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+
+    const [loading, setLoading] = useState(false);
+
+    const submit = e => {
+        e.preventDefault();
+
+        const data = {title, text};
+
+        setLoading(true);
+
+        axios.post('http://localhost:8000/blogs', data).then(response => {
+            console.log(response);
+        });
+
+        setLoading(false);
+
+        setTitle('');
+        setText('');
+    }
+
     return (
-        <p>New</p>
+        <div>
+            <form onSubmit={submit}>
+                <label className='form-label' htmlFor='title'>Blog title</label>
+                <input id='title' className='form-control' placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <br/>
+                <label className='form-label' htmlFor='text'>Blog text</label>
+                <textarea id='text' className='form-control' placeholder='Text' value={text} rows='10' onChange={(e) => setText(e.target.value)}/>
+                <br/>
+                {
+                    loading ? <button className='btn btn-dark' type='submit' disabled>Posting</button> : <button className='btn btn-dark' type='submit'>Post it</button>
+                }
+            </form>
+        </div>
     );
 }
 
