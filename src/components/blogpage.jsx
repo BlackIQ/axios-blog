68bbbbc382fc5719a81d33466d0dad51d3069f6f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-26 21:26:11 +0430

    Added delete

--- src/components/blogpage.jsx
+++ src/components/blogpage.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import BlogItem from "../components/blogitem";
 import {useParams} from "react-router-dom";
 
 const BlogPage = () => {
@@ -8,15 +7,30 @@ const BlogPage = () => {
 
     const {id} = useParams();
 
+    const deleteBlog = id => {
+        axios.delete('http://localhost:8000/blogs/' + id).then(response => {
+            console.log(response);
+        });
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:8000/blogs/' + id).then((response) => {setData(response.data)});
+        axios.get('http://localhost:8000/blogs/' + id).then((response) => {
+            setData(response.data)
+        });
     }, []);
+
+    if (!data) return null;
 
     return (
         <div>
-            {
-                data.name
-            }
+            <h1>{data.title}</h1>
+            <br/>
+            <p>{data.text}</p>
+            <hr />
+            <p>
+                <span onClick={(id) => {deleteBlog(data.id)}} className={'text-danger pointer'}>Delete</span>
+                <span onClick={(id) => {deleteBlog(data.id)}} className={'text-primary pointer float-end'}>Edit</span>
+            </p>
         </div>
     );
 }
